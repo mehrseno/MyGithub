56a456ec0f16fabab16c8b3ccd03f201f4f1be58
mehrseno <m.seno@aut.ac.ir>
2021-05-01 06:10:55 +0430

    Add script.js file and fetchData

---
+++ script.js
@@ -0,0 +1,23 @@
+document.querySelector('.transparent-search__submit-button').addEventListener('click', fetchData);
+
+async function fetchData() {
+    let username = document.querySelector('.transparent-search__input').value;
+    let str1 = "https://api.github.com/users/";
+    let url = str1.concat(String(username));
+    try {
+        let response = await fetch(url);
+        let data = await response.json();
+        console.log(data);
+        return data
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+async function getData() {
+    let mydata = await fetchData();
+    console.log(mydata.id)
+    return mydata
+}
+
+getData();
